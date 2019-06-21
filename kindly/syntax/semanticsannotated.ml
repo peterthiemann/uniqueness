5e9401bc8616f3e8a40f28c29b0072adfbc0e244
Peter Thiemann <peter.thiemann@gmail.com>
2019-06-21 16:51:26 +0200

    refined semantics and statement of theorem

--- kindly/syntax/semanticsannotated.ml
+++ kindly/syntax/semanticsannotated.ml
@@ -71,14 +71,14 @@ let s_rem : perm -> address -> perm = assert false
 
 (* end of syntax *)
 
-type 'a sem = Error of string | TimeOut | Return of 'a
+type 'a sem = Error of string | TimeOut | Ok of 'a
 
 let sembind : 'a sem -> ('a -> 'b sem) -> 'b sem =
   fun sa fsb ->
   match sa with
     Error (s) -> Error (s)
   | TimeOut -> TimeOut
-  | Return (a) -> fsb a
+  | Ok (a) -> fsb a
 
 let (let*) = sembind
 
@@ -86,13 +86,13 @@ let borrowed : address -> borrow -> address sem =
   fun (Address (bs, ell)) b ->
   match b with
     Imm ->
-     Return (Address (b :: bs, ell))
+     Ok (Address (b :: bs, ell))
   | Mut ->
      match bs with
        [] -> 
-        Return (Address ([b], ell))
+        Ok (Address ([b], ell))
      | (Mut :: bs) ->
-        Return (Address (b :: bs, ell))
+        Ok (Address (b :: bs, ell))
      | (Imm :: bs) ->
         Error ("trying to take mutable borrow of immutable borrow")
 
@@ -102,7 +102,7 @@ let rec vlookup : venv -> ident -> result sem =
   match gamma with
     VEmpty -> Error ("variable not found")
   | VUpd (gamma, y, r) ->
-     if x = y then Return r else vlookup gamma x
+     if x = y then Ok r else vlookup gamma x
 
 let (.!()) = vlookup
 
@@ -122,18 +122,18 @@ let salloc : store -> storable -> (raw_loc * store) sem =
   fun delta w ->
   match delta with
     Store (htable, nexta) ->
-      Return (nexta, Store (Store.add nexta w htable, nexta+1))
+      Ok (nexta, Store (Store.add nexta w htable, nexta+1))
 
 let slookup : store -> raw_loc -> storable sem =
   fun (Store (htable, _)) ell ->
   match Store.find_opt ell htable with
-  | Some x -> Return x
+  | Some x -> Ok x
   | None -> Error "illegal store location"
 
 let supdate : store -> raw_loc -> storable -> store sem =
   fun (Store (htable, limit)) ell w ->
   let htable' = Store.add ell w htable in
-  Return (Store (htable', limit))
+  Ok (Store (htable', limit))
 
 let (.![]) = slookup
 let (.![]<-) = supdate
@@ -141,49 +141,49 @@ let (.![]<-) = supdate
 let getraw_loc : result -> raw_loc sem =
   fun r ->
   match r with
-    RADDR (Address ([], rl)) -> Return rl
+    RADDR (Address ([], rl)) -> Ok rl
   | _ -> Error ("raw location expected")
 
 let getborrowed_loc : result -> (borrow * borrow list * raw_loc) sem =
   fun r ->
   match r with
-    RADDR (Address (b :: bs, ell)) -> Return (b, bs, ell)
+    RADDR (Address (b :: bs, ell)) -> Ok (b, bs, ell)
   | _ -> Error ("borrowed address expected")
 
 let getaddress : result -> address sem =
   fun r ->
   match r with
-    RADDR (rho) -> Return rho
+    RADDR (rho) -> Ok rho
   | _ -> Error ("address expected")
 
 let getstpoly : storable -> (venv * kappa list * constr * kind * ident * exp) sem =
   fun w ->
   match w with
-    STPOLY (gamma, kappas, cstr, knd, x, e) ->
-      Return (gamma, kappas, cstr, knd, x, e)
+    STPOLY (gamma, kappas, cstr, k, x, e) ->
+      Ok (gamma, kappas, cstr, k, x, e)
   | _ ->
      Error ("expected STPOLY")
 
 let getstclosure : storable -> (venv * kind * ident * exp) sem =
   fun w ->
   match w with
-    STCLOSURE (gamma, knd, x, e) ->
-    Return (gamma, knd, x, e)
+    STCLOSURE (gamma, k, x, e) ->
+    Ok (gamma, k, x, e)
   | _ ->
      Error ("expected STCLOSURE")
 
 let getstpair : storable -> (kind * result * result) sem =
   fun w ->
   match w with
-    STPAIR (knd, r1, r2) ->
-    Return (knd, r1, r2)
+    STPAIR (k, r1, r2) ->
+    Ok (k, r1, r2)
   | _ ->
      Error ("expected STPAIR")
 
 let getstresource : storable -> result sem =
   fun w ->
   match w with
-    STRESOURCE (r) -> Return r
+    STRESOURCE (r) -> Ok r
   | _ -> Error ("expected STRESOURCE")
 
 let (let*?) : bool -> (unit -> 'b sem) -> 'b sem =
@@ -200,140 +200,146 @@ let rec eval : store -> perm -> venv -> int -> exp -> (store * perm * result) se
   fun delta pi gamma i e ->
   if i=0 then TimeOut else
   match e with
+  (* rule const *)
   | CONST (c) -> 
-    Return (delta, pi, RCONST (c))
+    Ok (delta, pi, RCONST (c))
+  (**)
+  (* rule var *)
   | VAR (x) ->
     let* r = gamma.!(x) in
-    Return (delta, pi, r)
+    Ok (delta, pi, r)
+  (**)
   (* rule varinst *)
-  | VARINST (x, kinds, tys) ->
+  | VARINST (x, ks, tys) ->
     let* rx = gamma.!(x) in
     let* ell = getraw_loc rx in
     let*? () = List.mem (loc ell) pi in
     let* w = delta.![ell] in
-    let* (gamma', kappas', cstr', knd', x', e') = getstpoly w in
+    let* (gamma', kappas', cstr', k', x', e') = getstpoly w in
     let pi' =
-      if cstr'./{kinds --> kappas'} <=> [(knd', KUNR None)]./{kinds --> kappas'}  
+      if cstr'./{ks --> kappas'} <=> [(k', KUNR None)]./{ks --> kappas'}  
       then pi 
       else s_rem pi (loc ell)
     in
-    let w = STCLOSURE (gamma', knd'./{kinds --> kappas'},
-                       x', e'./{kinds --> kappas'}) in
+    let w = STCLOSURE (gamma', k'./{ks --> kappas'},
+                       x', e'./{ks --> kappas'}) in
     let* (ell', delta') = salloc delta w in
-    Return (delta', s_add pi' (loc ell'), RADDR (loc ell'))
+    Ok (delta', s_add pi' (loc ell'), RADDR (loc ell'))
   (**)
-  | POLYLAM (kappas, cstr, knd, x', e') ->
-     let w = STPOLY (gamma, kappas, cstr, knd, x', e') in
+  | POLYLAM (kappas, cstr, k, x', e') ->
+     let w = STPOLY (gamma, kappas, cstr, k, x', e') in
      let* (ell', delta') = salloc delta w in
-     Return (delta', s_add pi (loc ell'), RADDR (loc ell'))
-  | SAPP (e1, e2, split) ->
+     Ok (delta', s_add pi (loc ell'), RADDR (loc ell'))
+  (* rule sapp *)
+  | SAPP (e_1, e_2, split) ->
      let i' = i - 1 in
-     let (gamma1, gamma2) = vsplit gamma split in
-     let* (delta1, pi1, r1) = eval delta pi gamma1 i' e1 in
-     let* ell = getraw_loc r1 in
-     let* w = delta1.![ell] in
-     let* (gamma', knd', x', e') = getstclosure w in
-     let pi1' =
-       if knd' <= KUNR None then pi1 else s_rem pi1 (loc ell)
+     let (gamma_1, gamma_2) = vsplit gamma split in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma_1 i' e_1 in
+     let* ell = getraw_loc r_1 in
+     let* w = delta_1.![ell] in
+     let* (gamma', k', x', e') = getstclosure w in
+     let pi_1' =
+       if k' <= KUNR None then pi_1 else s_rem pi_1 (loc ell)
      in
-     let* (delta2, pi2, r2) = eval delta1 pi1' gamma2 i' e2 in
-     let* (delta3, pi3, r3) = eval delta2 pi2 (VUpd (gamma', x', r2)) i' e' in
-     Return (delta3, pi3, r3)
-  | SLET (x, e1, e2, split) ->
+     let* (delta_2, pi_2, r_2) = eval delta_1 pi_1' gamma_2 i' e_2 in
+     let* (delta_3, pi_3, r_3) = eval delta_2 pi_2 (VUpd (gamma', x', r_2)) i' e' in
+     Ok (delta_3, pi_3, r_3)
+  (**)
+  | SLET (x, e_1, e_2, split) ->
      let i' = i - 1 in
-     let (gamma1, gamma2) = vsplit gamma split in
-     let* (delta1, pi1, r1) = eval delta pi gamma1 i' e1 in
-     let* (delta2, pi2, r2) = eval delta1 pi1 (VUpd (gamma2, x, r1)) i' e2 in
-     Return (delta2, pi2, r2)
-  | SPAIR (knd, e1, e2, split) ->
+     let (gamma_1, gamma_2) = vsplit gamma split in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma_1 i' e_1 in
+     let* (delta_2, pi_2, r_2) = eval delta_1 pi_1 (VUpd (gamma_2, x, r_1)) i' e_2 in
+     Ok (delta_2, pi_2, r_2)
+  | SPAIR (k, e_1, e_2, split) ->
      let i' = i - 1 in
-     let (gamma1, gamma2) = vsplit gamma split in
-     let* (delta1, pi1, r1) = eval delta pi gamma1 i' e1 in
-     let* (delta2, pi2, r2) = eval delta1 pi1 gamma2 i' e2 in
-     let w = STPAIR (knd, r1, r2) in
+     let (gamma_1, gamma_2) = vsplit gamma split in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma_1 i' e_1 in
+     let* (delta_2, pi_2, r_2) = eval delta_1 pi_1 gamma_2 i' e_2 in
+     let w = STPAIR (k, r_1, r_2) in
      let* (ell', delta') = salloc delta w in
-     Return (delta2, s_add pi2 (loc ell'), RADDR (loc ell'))
-  | SMATCH (x, y, e1, e2, split) ->
+     Ok (delta_2, s_add pi_2 (loc ell'), RADDR (loc ell'))
+  | SMATCH (x, y, e_1, e_2, split) ->
      let i' = i - 1 in
-     let (gamma1, gamma2) = vsplit gamma split in
-     let* (delta1, pi1, r1) = eval delta pi gamma1 i' e1 in
-     let* ell = getraw_loc r1 in
-     let* w = delta1.![ell] in
-     let* (knd', r1', r2') = getstpair w in
-     let pi1' =
-       if knd' <= KUNR None then pi1 else s_rem pi1 (loc ell)
+     let (gamma_1, gamma_2) = vsplit gamma split in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma_1 i' e_1 in
+     let* ell = getraw_loc r_1 in
+     let* w = delta_1.![ell] in
+     let* (k', r_1', r_2') = getstpair w in
+     let pi_1' =
+       if k' <= KUNR None then pi_1 else s_rem pi_1 (loc ell)
      in
-     let* (delta2, pi2, r2) = eval delta1 pi1' (VUpd (VUpd (gamma2, x, r1'), y, r2')) i' e2 in
-     Return (delta2, pi2, r2)
-  | MATCHBORROW (x, y, e1, e2, split) ->
+     let* (delta_2, pi_2, r_2) = eval delta_1 pi_1' (VUpd (VUpd (gamma_2, x, r_1'), y, r_2')) i' e_2 in
+     Ok (delta_2, pi_2, r_2)
+  | MATCHBORROW (x, y, e_1, e_2, split) ->
      let i' = i - 1 in
-     let (gamma1, gamma2) = vsplit gamma split in
-     let* (delta1, pi1, r1) = eval delta pi gamma1 i' e1 in
-     let* (b, _, ell) = getborrowed_loc r1 in
-     let* w = delta1.![ell] in
-     let* (knd', r1', r2') = getstpair w in
-     let* rho1 = getaddress r1' in
-     let* rho2 = getaddress r2' in
-     let* rho1' = borrowed rho1 b in
-     let* rho2' = borrowed rho2 b in
-     let pi1' = s_add (s_add (s_rem (s_rem pi1 rho1) rho2) rho1') rho2' in
-     let r1'' = RADDR rho1' in
-     let r2'' = RADDR rho2' in
-     let* (delta2, pi2, r2) = eval delta1 pi1' (VUpd (VUpd (gamma2, x, r1''), y, r2'')) i' e2 in
-     let pi2' = s_add (s_add (s_rem (s_rem pi2 rho1') rho2') rho1) rho2 in
-     Return (delta2, pi2', r2)
-  | SREGION (e1, x, n, b) ->
+     let (gamma_1, gamma_2) = vsplit gamma split in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma_1 i' e_1 in
+     let* (b, _, ell) = getborrowed_loc r_1 in
+     let* w = delta_1.![ell] in
+     let* (k', r_1', r_2') = getstpair w in
+     let* rho_1 = getaddress r_1' in
+     let* rho_2 = getaddress r_2' in
+     let* rho_1' = borrowed rho_1 b in
+     let* rho_2' = borrowed rho_2 b in
+     let pi_1' = s_add (s_add (s_rem (s_rem pi_1 rho_1) rho_2) rho_1') rho_2' in
+     let r_1'' = RADDR rho_1' in
+     let r_2'' = RADDR rho_2' in
+     let* (delta_2, pi_2, r_2) = eval delta_1 pi_1' (VUpd (VUpd (gamma_2, x, r_1''), y, r_2'')) i' e_2 in
+     let pi_2' = s_add (s_add (s_rem (s_rem pi_2 rho_1') rho_2') rho_1) rho_2 in
+     Ok (delta_2, pi_2', r_2)
+  | SREGION (e_1, x, n, b) ->
      let i' = i - 1 in
      let* rx = gamma.!(x) in
      let* rho = getaddress rx in
      let*? () = List.mem rho pi in
      let* rho' = borrowed rho b in
      let pi' = s_add (s_rem pi rho) rho' in
-     let* (delta1, pi1, r1) = eval delta pi' gamma i' e1 in
-     let pi1' = s_add (s_rem pi rho') rho in
-     Return (delta1, pi1', r1)
+     let* (delta_1, pi_1, r_1) = eval delta pi' gamma i' e_1 in
+     let pi_1' = s_add (s_rem pi rho') rho in
+     Ok (delta_1, pi_1', r_1)
   | SBORROW (b, x) ->
      let* rx = gamma.!(x) in
      let* rho = getaddress rx in
      let* rho' = borrowed rho b in
      let*? () = List.mem rho' pi in
-     Return (delta, pi, RADDR rho')
-  | SCREATE (e1) ->
+     Ok (delta, pi, RADDR rho')
+  | SCREATE (e_1) ->
      let i' = i - 1 in
-     let* (delta1, pi1, r1) = eval delta pi gamma i' e1 in
-     let w = STRESOURCE (r1) in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma i' e_1 in
+     let w = STRESOURCE (r_1) in
      let* (ell', delta') = salloc delta w in
-     let pi1' = s_add pi (loc ell') in
-     Return (delta1, pi1', RADDR (loc ell'))
-  | SDESTROY (e1) ->
+     let pi_1' = s_add pi (loc ell') in
+     Ok (delta_1, pi_1', RADDR (loc ell'))
+  | SDESTROY (e_1) ->
      let i' = i - 1 in
-     let* (delta1, pi1, r1) = eval delta pi gamma i' e1 in
-     let* ell = getraw_loc r1 in
-     let* w = delta1.![ell] in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma i' e_1 in
+     let* ell = getraw_loc r_1 in
+     let* w = delta_1.![ell] in
      let* r = getstresource w in
-     let* delta1' = delta1.![ell] <- STRELEASED in
-     let pi1' = s_rem pi1 (loc ell) in
-     Return (delta1', pi1', RVOID)
-  | SOBSERVE (e1) ->
+     let* delta_1' = delta_1.![ell] <- STRELEASED in
+     let pi_1' = s_rem pi_1 (loc ell) in
+     Ok (delta_1', pi_1', RVOID)
+  | SOBSERVE (e_1) ->
      let i' = i - 1 in
-     let* (delta1, pi1, r1) = eval delta pi gamma i' e1 in
-     let* (b, _, ell) = getborrowed_loc r1 in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma i' e_1 in
+     let* (b, _, ell) = getborrowed_loc r_1 in
      let*? () = (b = Imm) in
-     let* w = delta1.![ell] in
+     let* w = delta_1.![ell] in
      let* r = getstresource w in
-     Return (delta1, pi1, r)
-  | SUPDATE (e1, e2, split) ->
+     Ok (delta_1, pi_1, r)
+  | SUPDATE (e_1, e_2, split) ->
      let i' = i - 1 in
-     let (gamma1, gamma2) = vsplit gamma split in
-     let* (delta1, pi1, r1) = eval delta pi gamma1 i' e1 in
-     let* rho = getaddress r1 in
-     let* (b, _, ell) = getborrowed_loc r1 in
+     let (gamma_1, gamma_2) = vsplit gamma split in
+     let* (delta_1, pi_1, r_1) = eval delta pi gamma_1 i' e_1 in
+     let* rho = getaddress r_1 in
+     let* (b, _, ell) = getborrowed_loc r_1 in
      let*? () = (b = Mut) in
-     let* (delta2, pi2, r2) = eval delta1 pi1 gamma2 i' e2 in
-     let* w = delta2.![ell] in
+     let* (delta_2, pi_2, r_2) = eval delta_1 pi_1 gamma_2 i' e_2 in
+     let* w = delta_2.![ell] in
      let* r = getstresource w in
-     let*? () = List.mem rho pi2 in
-     let* delta2' = delta2.![ell] <- STRESOURCE (r2) in
-     let pi2' = s_rem pi2 rho in
-     Return (delta2', pi2', RVOID)
+     let*? () = List.mem rho pi_2 in
+     let* delta_2' = delta_2.![ell] <- STRESOURCE (r_2) in
+     let pi_2' = s_rem pi_2 rho in
+     Ok (delta_2', pi_2', RVOID)
 
